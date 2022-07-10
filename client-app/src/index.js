e32c1c404d45a8f2c2dde07f30bc6403584ee2db
Eduardo Ayales <eduardoayalesleon@gmail.com>
2022-07-10 02:21:10 -0600

    fix(main) Update root to React 18

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
+
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
