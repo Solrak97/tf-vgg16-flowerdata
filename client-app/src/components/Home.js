d273e72504efc5926bfc98c2f78de3ad2bf1a90b
Eduardo Ayales <eduardoayalesleon@gmail.com>
2022-07-10 02:23:23 -0600

    feat(main) Show results

--- client-app/src/components/Home.js
+++ client-app/src/components/Home.js
@@ -1,21 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import DropZone from "./DropZone";
 import Button from "react-bootstrap/Button";
+import GaugeChart from 'react-gauge-chart'
 
 function Home() {
+
+  const [result, setResult] = useState({});
+  const [displayedResult, setDisplayResult] = useState(false);
+  const [imageUploaded, setImageUploaded] = useState(undefined);
+
+
+  const imageURL = imageUploaded ? URL.createObjectURL(imageUploaded) : "";
+
   return (
     <div>
-      <h1>Adjunta una imagen de una planta</h1>
-      <p class="small text-center">Recuerda que las categorías permitidas son: margaritas, rosas, dientes de león, tulipanes y girasoles</p>
-      <DropZone />
-      <br></br>
-      <Button
-        variant="primary"
-        style={{ backgroundColor: "#31525B", fontSize: "2rem" }}
-      >
-        Clasificar
-      </Button>{" "}
+      {displayedResult
+        ?
+        <div>
+          {result.error
+            ?
+            <div>
+              <h1 className="mb-5">Error al clasificar imagen</h1>
+              <Button
+                variant="primary"
+                style={{ backgroundColor: "#31525B", fontSize: "2rem" }}
+                onClick={() => { setDisplayResult(false); setImageUploaded(false); setResult({}); }}
+              >
+                Intentar de nuevo
+              </Button>
+            </div>
+            :
+            <div>
+              <h1 className="mb-5">Resultado de Clasificación</h1>
+
+              <div className="row justify-content-center w-100">
+                <div className="col-4 mb-5">
+                  <h2 className="mb-3">{result.payload.class}</h2>
+                  <img src={imageURL} alt="uploaded image" className="mx-auto" style={{ maxWidth: "55%", height: "auto", display: "block" }} />
+                </div>
+
+                <div className="col-4" style={{}}>
+                  <h2 className="mb-4">Accuracy</h2>
+                  <GaugeChart
+                    style={{ width: "110%", margin: "0 auto"}}
+                    nrOfLevels={20}
+                    percent={result.payload.accuracy / 100}
+                    needleColor="#345243"
+                    fontSize="2rem"
+                    textColor="#000000"
+                    colors={["#FF0000", "#00FF00"]}
+                    animateDuration={3000}
+                  />
+                </div>
+              </div>
+
+              <Button
+                variant="primary"
+                style={{ backgroundColor: "#31525B", fontSize: "2rem"}}
+                onClick={() => { setDisplayResult(false); setImageUploaded(false); setResult({}); }}
+              >
+                Clasificar otra imagen
+              </Button>
+            </div>
+          }
+        </div>
+        :
+        <div>
+          <h1>Adjunta una imagen de una planta</h1>
+          <p className="small text-center">Recuerda que las categorías permitidas son: margaritas, rosas, dientes de león, tulipanes y girasoles</p>
+          <DropZone setResult={setResult} setImageUploaded={setImageUploaded} />
+          <br></br>
+          <Button
+            variant="primary"
+            style={{ backgroundColor: "#31525B", fontSize: "2rem" }}
+            onClick={() => setDisplayResult(true)}
+            disabled={!imageUploaded}
+          >
+            Clasificar
+          </Button>
+        </div>
+      }
     </div>
   );
 }
